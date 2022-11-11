454d24bbcc339cb0e484779bba878a292e2adf73
cnhotr <hoang.tran@tuni.fi>
2022-11-11 15:36:09 +0200

    feat: updated the test cases

--- tests/compact.test.js
+++ tests/compact.test.js
@@ -1,4 +1,32 @@
-import add from '../src/compact';
+import compact from '../src/compact';
+
+// Test that compact should not remove any values if there are no falsey values
+test('compact should not remove any values if there are no falsey values', () => {
+    expect(compact([1, 2, 3])).toEqual([1, 2, 3]);
+});
+
+// Test that compact should remove all falsey values from an array
+test('compact should remove all falsey values from an array', () => {
+    expect(compact([0, 1, false, 2, '', 3, "", "4", null, 'string', NaN, undefined])).toEqual([1, 2, 3, "4", "string"]);
+});
+
+// Test that compact should return an empty array if the input array is empty
+test('compact should return an empty array if the input array is empty', () => {
+    expect(compact([])).toEqual([]);
+});
+
+// Test that compact should return an empty array if the input array is null
+test('compact should return an empty array if the input array is null', () => {
+    expect(compact(null)).toEqual([]);
+});
+
+// Test that compact should return an empty array if the input array is undefined
+test('compact should return an empty array if the input array is undefined', () => {
+    expect(compact(undefined)).toEqual([]);
+});
+
+
+
 
 
 
--- tests/eq.test.js
+++ tests/eq.test.js
@@ -1,4 +1,14 @@
-import add from '../src/eq';
+import eq from '../src/eq';
+
+// Test that eq should return true if the two values are equal
+test('eq should return true if the two values are equal', () => {
+    expect(eq(1, 1)).toEqual(true);
+});
+
+// Test that eq should return false if the two values are not equal
+test('eq should return false if the two values are not equal', () => {
+    expect(eq(1, 2)).toEqual(false);
+});
 
 
 
