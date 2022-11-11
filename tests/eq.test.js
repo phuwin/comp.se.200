0d4ed77668ace3bc4f031bb202be4a86b4198dd0
cnhotr <hoang.tran@tuni.fi>
2022-11-11 15:43:06 +0200

    feat: updated the test case for eq.js

--- tests/eq.test.js
+++ tests/eq.test.js
@@ -2,12 +2,37 @@ import eq from '../src/eq';
 
 // Test that eq should return true if the two values are equal
 test('eq should return true if the two values are equal', () => {
-    expect(eq(1, 1)).toEqual(true);
+  expect(eq(1, 1)).toEqual(true);
 });
 
 // Test that eq should return false if the two values are not equal
 test('eq should return false if the two values are not equal', () => {
-    expect(eq(1, 2)).toEqual(false);
+  expect(eq(1, 2)).toEqual(false);
+});
+
+// Test that eq should return false if the two values  types are not the same
+test('eq should return false if the two values types are not the same', () => {
+  expect(eq(1, '1')).toEqual(false);
+});
+
+// Test that eq should return true if the two values are NaN
+test('eq should return true if the two values are NaN', () => {
+  expect(eq(NaN, NaN)).toEqual(true);
+});
+
+// Test that eq should return true if the two objects are equal
+test('eq should return true if the two objects are equal', () => {
+  expect(eq({ a: 1 }, { a: 1 })).toEqual(true);
+});
+
+// Test that eq should return false if the two objects are not equal
+test('eq should return false if the two objects are not equal', () => {
+  expect(eq({ a: 1 }, { a: 2 })).toEqual(false);
+});
+
+// Test that eq should return false if comparing an object to a string or number
+test('eq should return false if comparing an object to a string or number', () => {
+  expect(eq({ a: 1 }, 1)).toEqual(false);
 });
 
 
