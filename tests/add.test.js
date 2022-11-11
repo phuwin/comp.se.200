6f25785e01e2d65eaba4fd90cf265de21b8b0d79
cnhotr <hoang.tran@tuni.fi>
2022-11-11 12:10:47 +0200

    fix: update the add.test.js

--- tests/add.test.js
+++ tests/add.test.js
@@ -1,51 +1,109 @@
 import add from '../src/add';
 
-// add 2 valid numbers
-test('add 2 valid numbers', () => {
-  expect(add(1, 2)).toBe(3);
+// Test add first number is positive and second number is positive
+test('add first number is positive and second number is positive', () => {
+  expect(add(1, 1)).toBe(2);
 });
 
-// add 2 invalid numbers
-test('add 2 invalid numbers', () => {
-  expect(add('a', 'b')).toBe(NaN);
+// Test add first number is positive and second number is negative
+test('add first number is positive and second number is negative', () => {
+  expect(add(1, -1)).toBe(0);
 });
 
-// add first number is valid and second number is invalid
-test('add 1 valid number and 1 invalid number', () => {
-  expect(add(1, 'b')).toBe(NaN);
+// Test add first number is negative and second number is positive
+test('add first number is negative and second number is positive', () => {
+  expect(add(-1, 1)).toBe(0);
 });
 
-// add first number is invalid and second number is valid
-test('add 1 valid number and 1 invalid number', () => {
-  expect(add('b', 1)).toBe(NaN);
+// Test add first number is negative and second number is negative
+test('add first number is negative and second number is negative', () => {
+  expect(add(-1, -1)).toBe(-2);
 });
 
-// add first number is valid and second number is undefined
-test('add 1 valid number and 1 undefined number', () => {
-  expect(add(1, undefined)).toBe(NaN);
+// Test add first number is positive and second number is zero
+test('add first number is positive and second number is zero', () => {
+  expect(add(1, 0)).toBe(1);
 });
 
-// add first number is undefined and second number is valid
-test('add 1 undefined number and 1 valid number', () => {
-  expect(add(undefined, 1)).toBe(NaN);
+// Test add first number is negative and second number is zero
+test('add first number is negative and second number is zero', () => {
+  expect(add(-1, 0)).toBe(-1);
 });
 
-// add first number is valid and second number is null
-test('add 1 valid number and 1 null number', () => {
-  expect(add(1, null)).toBe(1);
+// Test add first number is zero and second number is positive
+test('add first number is zero and second number is positive', () => {
+  expect(add(0, 1)).toBe(1);
 });
 
-// add first number is null and second number is valid
-test('add 1 null number and 1 valid number', () => {
+// Test add first number is zero and second number is negative
+test('add first number is zero and second number is negative', () => {
+  expect(add(0, -1)).toBe(-1);
+});
+
+// Test add first number is zero and second number is zero
+test('add first number is zero and second number is zero', () => {
+  expect(add(0, 0)).toBe(0);
+});
+
+// Test add 2 strings
+test('add 2 strings', () => {
+  expect(add('a', 'b')).toBe('ab');
+});
+
+// Test add 1 string and 1 number
+test('add 1 string and 1 number', () => {
+  expect(add('a', 1)).toBe('a1');
+});
+
+// Test add 1 number and 1 string
+test('add 1 number and 1 string', () => {
+  expect(add(1, 'a')).toBe('1a');
+});
+
+// Test add undefined and undefined, should return default value: 0
+test('add undefined and undefined', () => {
+  expect(add(undefined, undefined)).toBe(0);
+});
+
+// Test add undefined and 1, should return 1
+test('add undefined and 1', () => {
+  expect(add(undefined, 1)).toBe(1);
+});
+
+// Test add 1 and undefined, should return 1
+test('add 1 and undefined', () => {
+  expect(add(1, undefined)).toBe(1);
+});
+
+// Test add null and null, should return default value: 0
+test('add null and null', () => {
+  expect(add(null, null)).toBe(0);
+});
+
+// Test add null and 1, should return 1
+test('add null and 1', () => {
   expect(add(null, 1)).toBe(1);
 });
 
-// add first number is valid and second number is empty string
-test('add 1 valid number and 1 empty string', () => {
-  expect(add(1, '')).toBe(1);
+// Test add 1 and null, should return 1
+test('add 1 and null', () => {
+  expect(add(1, null)).toBe(1);
 });
 
-// add first number is empty string and second number is valid
-test('add 1 empty string and 1 valid number', () => {
-  expect(add('', 1)).toBe(1);
+// Test add empty string and empty string, should return default value: 0
+test('add empty string and empty string', () => {
+  expect(add('', '')).toBe('');
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
