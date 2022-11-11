36687d56ede735d8a12df98580ba916d5b3adda2
cnhotr <hoang.tran@tuni.fi>
2022-11-11 16:19:03 +0200

    feat: added test case for filter.js

--- tests/filter.test.js
+++ tests/filter.test.js
@@ -1,4 +1,39 @@
-import add from '../src/filter';
+import filter from '../src/filter';
+
+// Test that filter should return an array of all elements predicate returns truthy for 
+test('filter should return an array of all elements predicate returns truthy for', () => {
+    const users = [
+        { 'user': 'barney', 'active': true },
+        { 'user': 'fred', 'active': false }
+    ];
+
+    expect(filter(users, ({ active }) => active)).toEqual([{ 'user': 'barney', 'active': true }]);
+});
+
+// Test that filter should return an empty array if the predicate returns false for all elements
+test('filter should return an empty array if the predicate returns false for all elements', () => {
+    const users = [
+        { 'user': 'barney', 'active': true },
+        { 'user': 'fred', 'active': false }
+    ];
+
+    expect(filter(users, ({ active }) => !active)).toEqual([]);
+});
+
+// Test that filter should return an empty array if the collection is not an array or object
+test('filter should return an empty array if the collection is not an array or object', () => {
+    expect(filter(1, () => true)).toEqual([]);
+});
+
+// Test that filter should return an empty array if the predicate is not a function
+test('filter should return an empty array if the predicate is not a function', () => {
+    expect(filter([1, 2, 3], 1)).toEqual([]);
+});
+
+// Test that filter should return an empty array if the predicate value is not available
+test('filter should return an empty array if the predicate value is not available', () => {
+    expect(filter([1, 2, 3], () => { })).toEqual([]);
+});
 
 
 
